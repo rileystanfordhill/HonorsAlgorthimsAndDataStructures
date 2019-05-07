c0e59483fd1966cacb51c1bc4a7987a4d7367d10
rileystanfordhill <42977596+rileystanfordhill@users.noreply.github.com>
2019-05-06 17:35:18 -0700

    Create FibObfuscated.java

---
+++ Homework/FibObfuscated.java
@@ -0,0 +1,91 @@
+
+public class FibbonacciObfuscated {
+
+	public static void main(String[] args) {
+
+		char [] ahfafs76162b = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+		String [] shfgwyfbf = {"oMRQJ*{av9mEW<:9yeMc,{ZJC;`W[ZoNLY?3%72{OGx@N%K>o]R80%EK48FC1fr2", 
+				"?)lQ\\Z&0)`-09m5^:*BS7o#y\"#DRmSR*}H{bgQ$/gSRl701r3:4!aT5#qYkQv'Ov", 
+				"j$)M;0dkqjDb0nmlCR\\<v1RsuLr3u)#DSzl>].DTpRQhrv!/<i`d$kjh`94(:NGV", 
+				"/21wrkqe?]'kjh&NwzFX*Gh)O#m3ntN+qM?5=>Fb5|${uKI]xdYF{3&[F5/8-fS4H", 
+				"6f4]K>ZcK[>sjc38#Rjh@{;PAgUEvHWh%iWY_wGM5ObnTe1|mWM<O\\4h4enlO\"[]", 
+				"N2V&JUg':]Td:(8f\\G5KL,MpQ\"^-YQy:m\\j-:qpMp5N\\`giD.u6V(|c.o&-{+p", 
+				"B\"S@G[sC2UJ2_8<cDR9FK4)UI)^Rn/HKPFl@Qcp6[H4<3z4V!8Zt>QAI", 
+				"LcwcRj~Of<$Tln?S>!I%8VufY3nh.ffTLeKop)OUN$zzivB@b:}",
+				"@Cdth*01WP6|dZF*D:@^[odjS)k`&EfU(OP+gZ",
+				"_,4$E.l5F<RJ%|)J};;", 
+				"N+qM?5=>Fb5|${uKI]xdj8afyw2312732hwg1ahahsdfh", 
+				"ahsjkdfhsuifhwiuefwdh23726onflasjfafsfsfaqpMp5N\\\\\\\\`giD.u6V(|c.o&", 
+				"pQ\\\"^-YQy:m\\\\j-:qpMp5N\\\\`giD.u6V(|c.o&-{+p", 
+				")UI)^Rn/HKPFl@Qcp6[H4<3z4V!8Zt>QAIf\\%iWY_w\\G5KL,MpQ", 
+				"iWY_wGM5ObnTe1|mWM<O\\09m5^:*BS7o#y\\d:(8f\\\\G5KL,MpQ\\\"^-Y", 
+				"OUN$zzivB@", 
+				"'kjh&NwzFX*Gh)O#m3ntN+qM?5=>Fb5|${uKI]xdYF{\""}; 
+
+		try { 
+			String asjhfaywbfuwbf = "(?i)(<title.*?>)(.+?)()";
+			int i = (int)'C' - (int)'C'; 
+			while (i < ahfys72h2h(shfgwyfbf)) { 
+				shfgwyfbf[i].matches(asjhfaywbfuwbf);
+				i += jshafuahiufhwf("^4"); 
+			}
+		} catch (Exception e) { 
+
+		} finally { 
+			asjfdbuawefsha(shfgwyfbf, ahfafs76162b); 
+		}
+	}
+
+	public static int asjfdbuawefsha(String [] ajshfiasfd723, char [] asfhyawheaf) { 
+		int asfuhsf22 = (int)'C' - 'C';
+		int kahfhw8723 = (int)'A'; 
+		while (kahfhw8723 < (int)'K') {
+			System.out.print((int)asfhyawheaf[asfuhsf22] - jshafuahiufhwf(ajshfiasfd723[asfuhsf22])+ " " );
+			kahfhw8723 += jshafuahiufhwf("A");
+			asfuhsf22 += jshafuahiufhwf("A");
+		}
+		return kahfhw8723; 
+	}
+
+	public static int jshafuahiufhwf(String ansfuhwa732) { 
+		int jahsfashfsjdfhx = (int)' ' - (int)' '; 
+		String adfhasjfhsa = "askjfhljsfbhsbfial"; 
+		try { 
+			while (jahsfashfsjdfhx < Integer.MAX_VALUE) { 
+				adfhasjfhsa += ansfuhwa732.charAt(jahsfashfsjdfhx); 
+				jahsfashfsjdfhx += (int)'B' - (int)'A'; 
+			}
+		} catch (Exception e) { 
+
+		} finally { 
+			try { 
+				adfhasjfhsa.matches("[a-z&&[^m-p]&&1-10&&/d]"); 
+			} catch (Exception e) { 
+
+			}
+		}
+		return jahsfashfsjdfhx; 
+	}
+
+	public static int ahfys72h2h (String[] jahfhsdf762) { 
+		int kjahfgsydf63 = (int)' ' - (int)' '; 
+		String jhgy3722hh = "l8)(&{}jsfbhsbfial"; 
+		try { 
+			while (kjahfgsydf63 < Integer.MAX_VALUE) { 
+				jhgy3722hh += jahfhsdf762[kjahfgsydf63]; 
+				kjahfgsydf63 += (int)'Z' - (int)'Y'; 
+			}
+		} catch (Exception e) { 
+
+		} finally { 
+			try { 
+				jhgy3722hh.matches("(0|(1(01*0)*1))*"); 
+			} catch (Exception e) { 
+				
+			}
+		}
+		return kjahfgsydf63; 
+	}
+
+}
